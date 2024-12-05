bcee665b7b08939936373ab4f94e32bc72e9588b
theselegadev <matheus.seleghin7@gmail.com>
2024-12-05 12:00:19 -0300

    [aula funções] novo arquivo com funções criadas no php

---
+++ funcoes.php
@@ -0,0 +1,20 @@
+<?php
+    // funções nativas
+    // count();
+    // rand(-150);
+    // echo "";
+    // print_r();
+    // array();
+
+    function quebrar_linha(){
+        echo "<br>";
+    }
+
+    function imprimir_texto($texto){
+        echo $texto;
+    }
+
+    function soma($n1,$n2){
+        echo $n1 + $n2;
+    }
+?>
