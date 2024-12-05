22833a22b7150e490627fe4ccdc86c615bfd1023
theselegadev <matheus.seleghin7@gmail.com>
2024-12-05 12:05:09 -0300

    [aula funções] chamando as funções criadas no php

--- funcoes.php
+++ funcoes.php
@@ -14,7 +14,15 @@
         echo $texto;
     }
 
+    imprimir_texto("Hello world");
+
+    quebrar_linha();
+
     function soma($n1,$n2){
         echo $n1 + $n2;
     }
+
+    soma(10,10);
+
+    quebrar_linha();
 ?>
