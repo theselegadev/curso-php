22a241a403eea624eff80a95aaaaa336c916b597
theselegadev <matheus.seleghin7@gmail.com>
2024-12-20 15:34:06 -0300

    [switch case] switch case no php

---
+++ switch_case.php
@@ -0,0 +1,21 @@
+<?php
+
+    $cor = "vermelho";
+
+    switch($cor){
+        case "vermelho":
+            echo "Sua cor preferida é vermelho";
+            break;
+        case "amarelo":
+            echo "Sua cor preferida é amarelo";
+            break;
+        case "azul":
+            echo "Sua cor preferida é azul";
+            break;
+        case "branco":
+            echo "Sua cor preferida é branco";
+            break;
+        default:
+            echo "Sua cor não está listada";
+    }
+?>
