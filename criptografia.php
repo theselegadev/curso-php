63294d992bae04dfd97036a07330bfe5030b860f
theselegadev <matheus.seleghin7@gmail.com>
2024-12-28 11:44:38 -0300

    [criptografia] base64, md5, sha1 no php

---
+++ criptografia.php
@@ -0,0 +1,18 @@
+<?php 
+    // criptografia/codificação
+
+    $senha = "123456";
+
+    // base64
+    $novasenha = base64_encode($senha);
+    echo "<h2>Base64:</h2><br>";
+    echo "base64: ".$novasenha."<br>Senha: ".base64_decode($novasenha)."<br>";
+    echo "<hr>";
+    // md5
+    echo "<h2>Md5:</h2><br>";
+    echo "md5: ".md5($senha). "<br>Senha: ".$senha."<br>";
+    echo "<hr>";
+    // sha1
+    echo "<h2>Sha1</h2><br>";
+    echo "sha1: ".sha1($senha)."<br>Senha: ".$senha."<br>";
+?>
