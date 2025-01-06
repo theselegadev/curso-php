4f0f3626d5f7d0272c54ce7c42367efc5ee1aca9
theselegadev <matheus.seleghin7@gmail.com>
2025-01-06 17:33:17 -0300

    [CRUD] Read

--- CRUD/index.php
+++ CRUD/index.php
@@ -1,3 +1,6 @@
+<?php 
+    include_once "./conexao.php";
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -16,13 +19,22 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Matheus</td>
-                <td>Seleghin</td>
-                <td>Matheus@.com.br</td>
-                <td><a href="#">Editar</a></td>
-                <td><a href="#">Remover</a></td>
-            </tr>
+            <?php
+                $query = "select * from clientes";
+
+                $resultado = mysqli_query($conexao,$query);
+
+                
+                while($dados = mysqli_fetch_array($resultado)){
+            ?>
+                <tr>
+                    <td><?php echo $dados['nome'];?></td>
+                    <td><?php echo $dados['sobrenome'];?></td>
+                    <td><?php echo $dados['email']?></td>
+                    <td><a href="#">Editar</a></td>
+                    <td><a href="#">Remover</a></td>
+                </tr>
+                <?php }?>
         </tbody>
     </table>
     <a href="./adicionar.php">Adicionar cliente</a>
