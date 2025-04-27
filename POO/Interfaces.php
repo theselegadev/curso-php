2f0680ba80fa49ec2f835429f65f826fe940b95b
theselegadev <matheus.seleghin7@gmail.com>
2025-04-27 13:02:00 -0300

    [PHP OO] Interfaces

---
+++ POO/Interfaces.php
@@ -0,0 +1,24 @@
+<?php
+    
+    interface Crud{
+        public function create();
+        public function read();
+        public function update();
+        public function delete();
+    }
+
+    class Noticias implements Crud{
+        public function create(){
+            // lógica
+        }
+        public function read(){
+            // lógica 
+        }
+        public function update(){
+            // lógica
+        }
+        public function delete(){
+            // lógica
+        }
+    }
+?>
