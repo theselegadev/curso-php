5c20d941cfa5825f783b62508b7e90f85d2a24ca
theselegadev <matheus.seleghin7@gmail.com>
2025-05-09 14:57:19 -0300

    [PHP OO] Testando o composer, composer init, require e update

---
+++ POO/composer-test/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'selegadev/composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5647da3bbc1e0de6c1adb2dbd56ec919f87bc69d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pagseguro/pagseguro-php-sdk' => array(
+            'pretty_version' => '4.2.1',
+            'version' => '4.2.1.0',
+            'reference' => '3e164aff66fa089eb79a80ce6b7413fe36aa9b95',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pagseguro/pagseguro-php-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'selegadev/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5647da3bbc1e0de6c1adb2dbd56ec919f87bc69d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
